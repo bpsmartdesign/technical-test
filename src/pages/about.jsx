c72db17cce6120281670ba57747f4d248ec60fdb
BIYA Paul <bpsmartdesign@hotmail.com>
2023-01-20 08:15:38 +0100

    chore: Add, config and test `tailwind css`.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,8 +14,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <p>lorem ipsum</p>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
     </>
-  )
+  );
 }
